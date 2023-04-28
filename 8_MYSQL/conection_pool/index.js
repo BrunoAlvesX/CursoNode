4abca7c863abe8ef0be5ff7ce3182d46da06f29a
Bruno <38993322+NewarkX@users.noreply.github.com>
2023-04-28 10:40:32 -0300

    connection pool

---
+++ 8_MYSQL/conection_pool/index.js
@@ -0,0 +1,118 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const  pool = require('./db/conn')
+
+const app = express()
+
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+)
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+app.use(express.static('public'))
+
+app.post('/books/insertbook', (request,response) => {
+    const title = request.body.title
+    const pageqty = request.body.pageqty
+
+    const sql = `INSERT INTO books (title,pageqty) VALUES ('${title}','${pageqty}')`
+
+    pool.query(sql, function(err){
+        if(err){
+            console.log(err)
+            return
+        }
+
+        response.redirect('/')
+    })
+})
+
+app.get('/books', (request,response) => {
+    const sql = "SELECT * FROM books"
+
+    pool.query(sql, function(err,data){
+        if(err){
+            console.log(err)
+            return
+        }
+
+        const books = data
+
+        response.render('books', { books })
+    })
+})
+
+app.get('/books/:id', (request,response) => {
+
+    const id = request.params.id
+    const sql = `SELECT * FROM books WHERE id = ${id}`
+
+    pool.query(sql, function(err,data){
+        if(err){
+            console.log(err)
+            return
+        }
+
+        const book = data[0]
+
+        response.render('book', { book })
+    })
+})
+
+
+app.get('/books/edit/:id', (request,response) => {
+
+    const id = request.params.id
+    const sql = `SELECT * FROM books WHERE id = ${id}`
+
+    pool.query(sql, function(err,data){
+        if(err){
+            console.log(err)
+            return
+        }
+
+        const book = data[0]
+
+        response.render('editbook', { book })
+    })
+})
+
+app.post('/books/updatebook', (request,response) => {
+    const id = request.body.id
+    const title = request.body.title
+    const pageqty = request.body.pageqty
+
+    const sql = `UPDATE books SET title = '${title}', pageqty = '${pageqty}' WHERE id = ${id}`
+
+    pool.query(sql, function(err){
+        if(err){
+            console.log(err)
+            return
+        }
+
+        response.redirect('/books')
+    })
+})
+
+app.post('/books/remove/:id',(request,response) => {
+    const id = request.params.id
+    const sql = `DELETE FROM books WHERE id = ${id}`
+
+    pool.query(sql, function(err){
+        if(err){
+            console.log(err)
+            return
+        }
+        response.redirect('/books')
+    })
+})
+
+app.get('/', (request,response) => {
+    response.render('home')
+})
+
+app.listen(3000)
