9d4a13823a1f510b94c1e7f4b3c3f0dcbe314fde
Bruno <38993322+NewarkX@users.noreply.github.com>
2023-06-02 09:52:19 -0300

    create tasks ok

---
+++ 10_MVC/salvando dados/controllers/TaskController.js
@@ -0,0 +1,23 @@
+const Task = require('../models/Task')
+
+module.exports = class TaskController{
+    static createTask(request, response){
+        response.render('tasks/create')
+    }
+
+    static async createTaskSave(request, response){
+        const task  = {
+            title: request.body.title,
+            description: request.body.description,
+            done: false
+        }
+        
+        await Task.create(task)
+
+        response.redirect('/tasks')
+    }
+
+    static showTasks(request, response){
+        response.render('tasks/all')
+    }
+}
