f0daf70f6a4a52fc502d0b9e100801c2a5c08e83
Bruno <38993322+NewarkX@users.noreply.github.com>
2023-06-05 10:04:17 -0300

    exibindo dados

---
+++ 10_MVC/exibindo dados/controllers/TaskController.js
@@ -0,0 +1,26 @@
+const Task = require('../models/Task')
+
+module.exports = class TaskController{
+    static createTask(request, response){
+        response.render('tasks/create')
+    }
+
+    static async createTaskSave(request, response){
+        const task  = {
+            title: request.body.title,
+            description: request.body.description,
+            done: false
+        }
+        
+        await Task.create(task)
+
+        response.redirect('/tasks')
+    }
+
+    static async showTasks(request, response){
+
+        const tasks = await Task.findAll({raw: true})
+
+        response.render('tasks/all' , {tasks})
+    }
+}
