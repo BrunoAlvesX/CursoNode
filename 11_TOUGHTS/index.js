2fc4568f65c793a18448fa929a90e897500c63c2
Bruno <38993322+NewarkX@users.noreply.github.com>
2023-06-14 12:33:50 -0300

    setup pronto

--- 11_TOUGHTS/index.js
+++ 11_TOUGHTS/index.js
@@ -8,6 +8,55 @@ const app = express()
 
 const conn = require('./db/conn')
 
+//template engine
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+//receber resposta do body
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+)
+
+//para receber o dado em json
+app.use(express.json)
+
+//session middleware
+app.use(
+    session({
+        name: "session",
+        secret: "nosso_secret",
+        resave: false,
+        saveUninitialized: false,
+        store: new FileStore({
+            logFn: function() {},
+            path: require('path').join(require('os').tmpdir(),'sessions'),
+        }),
+        cookie: {
+            secure: false,
+            maxAge: 360000,
+            expires: new Date(Date.now() + 360000),
+            httpOnly: true
+        }
+    })
+)
+
+//flash messages
+app.use(flash())
+
+//arquivos publicos
+app.use(express.static('public'))
+
+//salvar a sessao da resposta
+app.use((request,response,next) =>{ //caso o usuario n esteja logado ele da um next
+    if(request.session.userid){ //se o usuario estiver logado manda a sessao que esta na requisicao oara a resposta
+        response.locals.session = request.session
+    }
+
+    next()
+})
+
 conn
     .sync()
     .then(() => {
